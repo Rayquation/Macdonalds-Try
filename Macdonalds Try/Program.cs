990af651957e22b3e628a1d6b3db075aa9ac9b06
Emil Isak Lundqvist <emil372r@elev.tec.dk>
2022-04-08 12:24:03 +0200

    Done FUCK THIS BITCH

--- Macdonalds Try/Program.cs
+++ Macdonalds Try/Program.cs
@@ -9,41 +9,10 @@ namespace MacDonalds_Project
 {
     internal class Program
     {
-
+        public static int[] valg = new int[3];
         static void Main(string[] args)
         {
-            try
-            {
-                int[] choice = new int[3];
-                string con = "y";
-                int customer = 1;
-                do
-                {
-                    Console.WriteLine("Velkommen Til Macdonalds");
-                    choice[0] = FoodMenu();
-                    choice[1] = DiverseMenu();
-                    choice[2] = DrikMenu();
-                    Console.WriteLine($"Du skal i alt betale {choice.Sum():c}");
-                    Console.WriteLine($"efter du har betalt har du {1000 - choice.Sum():c} tilbage");
-                    Console.WriteLine($"Du er nr.{customer} i køen");
-                    WaitTime(customer);
-                    Console.WriteLine("Vil du gerne fortsætte {y/n}");
-                    if (con != "n")
-                    {
-                        customer++;
-                    }
-                    con = Console.ReadLine();
-                    con = con.ToLower();
-                }
-                while (con != "n");
-                Console.WriteLine("Tryk på hvilken som helst knap når du har fået din bestilling");
-                Console.ReadKey();
-            }
-            catch
-            {
-                Main(args);
-            }
-
+            Menu(args);
         }
         static int FoodMenu()
         {
@@ -111,14 +80,116 @@ namespace MacDonalds_Project
                 int random = rand.Next(100);
                 await Task.Delay(random * 1000);
                 Console.WriteLine($"Din mad er klar {customer} og det tog {random} sekunder");
-                MarioKart();
                 Thread.Sleep(5000);
             };
             sleep.TimeLimit();
         }
-        public static void MarioKart()
+        public static void Menu(string[] args)
         {
-            Console.Beep(659, 125); Console.Beep(659, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(523, 125); Console.Beep(659, 125); Thread.Sleep(125); Console.Beep(784, 125); Thread.Sleep(375); Console.Beep(392, 125); Thread.Sleep(375); Console.Beep(523, 125); Thread.Sleep(250); Console.Beep(392, 125); Thread.Sleep(250); Console.Beep(330, 125); Thread.Sleep(250); Console.Beep(440, 125); Thread.Sleep(125); Console.Beep(494, 125); Thread.Sleep(125); Console.Beep(466, 125); Thread.Sleep(42); Console.Beep(440, 125); Thread.Sleep(125); Console.Beep(392, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(125); Console.Beep(784, 125); Thread.Sleep(125); Console.Beep(880, 125); Thread.Sleep(125); Console.Beep(698, 125); Console.Beep(784, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(125); Console.Beep(523, 125); Thread.Sleep(125); Console.Beep(587, 125); Console.Beep(494, 125); Thread.Sleep(125); Console.Beep(523, 125); Thread.Sleep(250); Console.Beep(392, 125); Thread.Sleep(250); Console.Beep(330, 125); Thread.Sleep(250); Console.Beep(440, 125); Thread.Sleep(125); Console.Beep(494, 125); Thread.Sleep(125); Console.Beep(466, 125); Thread.Sleep(42); Console.Beep(440, 125); Thread.Sleep(125); Console.Beep(392, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(125); Console.Beep(784, 125); Thread.Sleep(125); Console.Beep(880, 125); Thread.Sleep(125); Console.Beep(698, 125); Console.Beep(784, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(125); Console.Beep(523, 125); Thread.Sleep(125); Console.Beep(587, 125); Console.Beep(494, 125); Thread.Sleep(375); Console.Beep(784, 125); Console.Beep(740, 125); Console.Beep(698, 125); Thread.Sleep(42); Console.Beep(622, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(415, 125); Console.Beep(440, 125); Console.Beep(523, 125); Thread.Sleep(125); Console.Beep(440, 125); Console.Beep(523, 125); Console.Beep(587, 125); Thread.Sleep(250); Console.Beep(784, 125); Console.Beep(740, 125); Console.Beep(698, 125); Thread.Sleep(42); Console.Beep(622, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(698, 125); Thread.Sleep(125); Console.Beep(698, 125); Console.Beep(698, 125); Thread.Sleep(625); Console.Beep(784, 125); Console.Beep(740, 125); Console.Beep(698, 125); Thread.Sleep(42); Console.Beep(622, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(415, 125); Console.Beep(440, 125); Console.Beep(523, 125); Thread.Sleep(125); Console.Beep(440, 125); Console.Beep(523, 125); Console.Beep(587, 125); Thread.Sleep(250); Console.Beep(622, 125); Thread.Sleep(250); Console.Beep(587, 125); Thread.Sleep(250); Console.Beep(523, 125); Thread.Sleep(1125); Console.Beep(784, 125); Console.Beep(740, 125); Console.Beep(698, 125); Thread.Sleep(42); Console.Beep(622, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(415, 125); Console.Beep(440, 125); Console.Beep(523, 125); Thread.Sleep(125); Console.Beep(440, 125); Console.Beep(523, 125); Console.Beep(587, 125); Thread.Sleep(250); Console.Beep(784, 125); Console.Beep(740, 125); Console.Beep(698, 125); Thread.Sleep(42); Console.Beep(622, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(698, 125); Thread.Sleep(125); Console.Beep(698, 125); Console.Beep(698, 125); Thread.Sleep(625); Console.Beep(784, 125); Console.Beep(740, 125); Console.Beep(698, 125); Thread.Sleep(42); Console.Beep(622, 125); Thread.Sleep(125); Console.Beep(659, 125); Thread.Sleep(167); Console.Beep(415, 125); Console.Beep(440, 125); Console.Beep(523, 125); Thread.Sleep(125); Console.Beep(440, 125); Console.Beep(523, 125); Console.Beep(587, 125); Thread.Sleep(250); Console.Beep(622, 125); Thread.Sleep(250); Console.Beep(587, 125); Thread.Sleep(250); Console.Beep(523, 125);
+            Console.WriteLine($"Velkommen til Macdonalds Menuen");
+            Console.WriteLine("Vælg hvilken menu du gerne vil ind til");
+            string[] Menus = { "MadMenu", "DiverseMenu", "DrikMenu", "ÆndreBestiling", "Betal" };
+            int i = 1;
+            int tmp = 0;
+            foreach (string item in Menus)
+            {
+                Console.WriteLine($"{i}. {item}");
+                tmp++;
+                i++;
+            }
+            int choice = Convert.ToInt32(Console.ReadLine());
+            int ran = choice - 1;
+            Console.WriteLine($"Du har valgt {Menus[ran]}");
+            try
+            {
+                string con = "y";
+                int customer = 1;
+                int change = 0;
+                do
+                {
+                        if (choice == 1)
+                        {
+                        valg[0] = FoodMenu();
+                        Menu(args);
+                        }
+                        else if (choice == 2)
+                        {
+                        valg[1] = DiverseMenu();
+                        Menu(args);
+                        }
+                        else if (choice == 3)
+                        {
+                        valg[2]=DrikMenu();
+                            Menu(args);
+                        }
+                        else if (choice == 4)
+                        {
+                            change = MenuChange(valg);
+                        if (change == 1)
+                        {
+                            valg[0] = FoodMenu();
+                            Menu(args);
+                        }else if (change == 2)
+                        {
+                            valg[1] = DiverseMenu();
+                            Menu(args);
+                        }
+                        else
+                        {
+                            valg[2] = DrikMenu();
+                            Menu(args);
+                        }
+                        }
+                        else
+                        {
+                            customer = Pay(valg);
+                            Menu(args);
+                        }
+                }
+                while (con != "n");
+                Console.WriteLine("Tryk på hvilken som helst knap når du har fået din bestilling");
+                Console.ReadKey();
+            }
+            catch
+            {
+                Main(args);
+            }
+        }
+        public static int MenuChange(int[] x)
+        {
+            Console.WriteLine("Hvad vil du gerne ændre i din menu");
+            Console.WriteLine("Vælg hvilken menu du gerne vil ind til");
+            string[] Menus = { "MadMenu", "DiverseMenu", "DrikMenu" };
+            int i = 1;
+            int tmp = 0;
+            foreach (string item in Menus)
+            {
+                Console.WriteLine($"{i}. {item}");
+                tmp++;
+                i++;
+            }
+            int choice = Convert.ToInt32(Console.ReadLine());
+            int ran = choice - 1;
+            Console.WriteLine($"Du har valgt {Menus[ran]}");
+            return choice;
+
+        }
+        public static int Pay(int[] x)
+        {
+            string con = "y";
+            int customer = 1;
+            Console.WriteLine($"Du skal i alt betale {x.Sum():c}");
+            Console.WriteLine($"efter du har betalt har du {1000 - x.Sum():c} tilbage");
+            Console.WriteLine($"Du er nr.{customer} i køen");
+            WaitTime(customer);
+            Console.WriteLine("Vil du gerne fortsætte {y/n}");
+            if (con != "n")
+            {
+                customer++;
+            }
+            con = Console.ReadLine();
+            con = con.ToLower();
+            return customer;
         }
     }
     public class EventMenuSleep
