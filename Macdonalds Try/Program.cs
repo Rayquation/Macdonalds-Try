19d00594f4bdaa894c9c8454926ef8fd96b92437
Emil Isak Lundqvist <emil372r@elev.tec.dk>
2022-04-08 13:08:47 +0200

    I AM FINALLY FINISHED

--- Macdonalds Try/Program.cs
+++ Macdonalds Try/Program.cs
@@ -10,9 +10,17 @@ namespace MacDonalds_Project
     internal class Program
     {
         public static int[] valg = new int[3];
+        public static string con = "y";
+        public static int customer = 1;
+        public static int change = 0;
         static void Main(string[] args)
         {
-            Menu(args);
+            do
+            {
+                con =Menu();
+            }while (con == "y");
+            Console.WriteLine("Tryk på hvilken som helst knap når du har fået din bestilling");
+            Console.ReadKey();
         }
         static int FoodMenu()
         {
@@ -84,77 +92,65 @@ namespace MacDonalds_Project
             };
             sleep.TimeLimit();
         }
-        public static void Menu(string[] args)
+        public static string Menu()
         {
-            Console.WriteLine($"Velkommen til Macdonalds Menuen");
-            Console.WriteLine("Vælg hvilken menu du gerne vil ind til");
-            string[] Menus = { "MadMenu", "DiverseMenu", "DrikMenu", "ÆndreBestiling", "Betal" };
-            int i = 1;
-            int tmp = 0;
-            foreach (string item in Menus)
-            {
-                Console.WriteLine($"{i}. {item}");
-                tmp++;
-                i++;
-            }
-            int choice = Convert.ToInt32(Console.ReadLine());
-            int ran = choice - 1;
-            Console.WriteLine($"Du har valgt {Menus[ran]}");
-            try
-            {
-                string con = "y";
-                int customer = 1;
-                int change = 0;
-                do
+                Console.WriteLine($"Velkommen til Macdonalds Menuen");
+                Console.WriteLine("Vælg hvilken menu du gerne vil ind til");
+                string[] Menus = { "MadMenu", "DiverseMenu", "DrikMenu", "ÆndreBestiling", "Betal" };
+                int i = 1;
+                int tmp = 0;
+                foreach (string item in Menus)
                 {
-                        if (choice == 1)
-                        {
-                        valg[0] = FoodMenu();
-                        Menu(args);
-                        }
-                        else if (choice == 2)
-                        {
-                        valg[1] = DiverseMenu();
-                        Menu(args);
-                        }
-                        else if (choice == 3)
-                        {
-                        valg[2]=DrikMenu();
-                            Menu(args);
-                        }
-                        else if (choice == 4)
-                        {
-                            change = MenuChange(valg);
-                        if (change == 1)
-                        {
-                            valg[0] = FoodMenu();
-                            Menu(args);
-                        }else if (change == 2)
-                        {
-                            valg[1] = DiverseMenu();
-                            Menu(args);
-                        }
-                        else
-                        {
-                            valg[2] = DrikMenu();
-                            Menu(args);
-                        }
-                        }
-                        else
-                        {
-                            customer = Pay(valg);
-                            Menu(args);
-                        }
+                    Console.WriteLine($"{i}. {item}");
+                    tmp++;
+                    i++;
                 }
-                while (con != "n");
-                Console.WriteLine("Tryk på hvilken som helst knap når du har fået din bestilling");
-                Console.ReadKey();
+                int choice = Convert.ToInt32(Console.ReadLine());
+                int ran = choice - 1;
+                Console.WriteLine($"Du har valgt {Menus[ran]}");
+                if (choice == 1)
+                {
+                    valg[0] = FoodMenu();
+                    Menu();
+                }
+                else if (choice == 2)
+                {
+                    valg[1] = DiverseMenu();
+                    Menu();
+                }
+                else if (choice == 3)
+                {
+                    valg[2] = DrikMenu();
+                    Menu();
+                }
+                else if (choice == 4)
+                {
+                    change = MenuChange(valg);
+                    if (change == 1)
+                    {
+                        valg[0] = FoodMenu();
+                        Menu();
+                    }
+                    else if (change == 2)
+                    {
+                        valg[1] = DiverseMenu();
+                        Menu();
+                    }
+                    else if (change == 3)
+                    {
+                        valg[2] = DrikMenu();
+                        Menu();
+                    }
+                }
+                else
+                {
+                    Pay(valg, customer);
+                    customer++;
+                    Console.WriteLine("Vil du gerne fortsætte? {y/n}");
+                    con = Console.ReadLine().ToLower();
+                }
+            return con;
             }
-            catch
-            {
-                Main(args);
-            }
-        }
         public static int MenuChange(int[] x)
         {
             Console.WriteLine("Hvad vil du gerne ændre i din menu");
@@ -174,23 +170,14 @@ namespace MacDonalds_Project
             return choice;
 
         }
-        public static int Pay(int[] x)
+        public static void Pay(int[] x, int customer)
         {
-            string con = "y";
-            int customer = 1;
             Console.WriteLine($"Du skal i alt betale {x.Sum():c}");
             Console.WriteLine($"efter du har betalt har du {1000 - x.Sum():c} tilbage");
             Console.WriteLine($"Du er nr.{customer} i køen");
             WaitTime(customer);
-            Console.WriteLine("Vil du gerne fortsætte {y/n}");
-            if (con != "n")
-            {
-                customer++;
-            }
-            con = Console.ReadLine();
-            con = con.ToLower();
-            return customer;
         }
+    }
     }
     public class EventMenuSleep
     {
@@ -201,4 +188,3 @@ namespace MacDonalds_Project
             Kitchen.Invoke(this, EventArgs.Empty);
         }
     }
-}
